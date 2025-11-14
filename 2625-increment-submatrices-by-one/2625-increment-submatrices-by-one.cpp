2496b60f43dc10ace15b3803e616558b1b7feab2
RITWIK RAI <rritwik1810@gmail.com>
2025-11-15 00:47:30 +0530

    Time: 1085 ms (14.11%), Space: 91.3 MB (47.34%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-    void difference_array(vector<vector<int>>&ans,int start_i,int start_j,int end_i,int end_j,int n)
+    void solve(vector<vector<int>>&ans,int start_i,int start_j,int end_i,int end_j)
     {
-
-         for(int i=start_i;i<=end_i;i++)
-         {
-            ans[i][start_j]+=1;
-         }
-
-         if(end_j+1<n)
-         {
-            for(int i=start_i;i<=end_i;i++)
+        for(int i=start_i;i<=end_i;i++)
+        {
+            for(int j=start_j;j<=end_j;j++)
             {
-                ans[i][end_j+1]-=1;
-            }  
-         }
-         
+                ans[i][j]+=1;
+            }
+        }
     }
     vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
 
@@ -31,15 +24,7 @@ public:
 
             int end_j=v[3];
 
-            difference_array(ans,start_i,start_j,end_i,end_j,n);
-        }
-
-        for(int i=0;i<n;i++)
-        {
-            for(int j=1;j<n;j++)
-            {
-                ans[i][j]+=ans[i][j-1];
-            }
+            solve(ans,start_i,start_j,end_i,end_j);
         }
 
         return ans;
