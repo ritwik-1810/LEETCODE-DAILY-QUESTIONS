becfa547d4c5a77d3755fd50c810783730d78e5c
RITWIK RAI <rritwik1810@gmail.com>
2025-11-16 00:33:25 +0530

    Time: 1501 ms (5.77%), Space: 685.4 MB (13.13%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.cpp
+++ 0696-count-binary-substrings/0696-count-binary-substrings.cpp
@@ -1,50 +1,54 @@
 class Solution {
 public:
+    int count(int i,int j,string s)
+    {
+          int cnt=0;
+
+          while(i>=0 && j<s.size() && s[i]=='1' && s[j]=='0')
+          {
+              cnt+=1;
+
+              i--;
+
+              j++;
+          }
+
+          return cnt;
+    }
+    int count1(int i,int j,string s)
+    {
+          int cnt=0;
+
+          while(i>=0 && j<s.size() && s[i]=='0' && s[j]=='1')
+          {
+              cnt+=1;
+
+              i--;
+
+              j++;
+          }
+
+          return cnt;
+    }
     int countBinarySubstrings(string s) {
 
-        vector<int>group;
-
-        int cnt=0;
-
-        int i=0;
-
-        while(i<s.size())
-        {
-
-            if(s[i]=='0')
-            {
-                while(s[i]=='0')
-                {
-                    cnt+=1;
-
-                    i++;
-                }
-
-                group.push_back(cnt);
-            }
-            else
-            {
-                while(s[i]=='1')
-                {
-                    cnt+=1;
-
-                    i++;
-                }
-
-                group.push_back(cnt);
-            }
-
-            cnt=0;
-        }
+        int size=s.size();
 
         int res=0;
 
-        for(int i=0;i<group.size()-1;i++)
+        for(int i=0;i<size-1;i++)
         {
-            res+=min(group[i],group[i+1]);
+             if(s[i]=='1' && s[i+1]=='0')
+             {
+                  res+=count(i,i+1,s);
+             }
+             else if(s[i]=='0' && s[i+1]=='1')
+             {
+                  res+=count1(i,i+1,s);
+             }
         }
 
         return res;
-        
+
     }
 };
