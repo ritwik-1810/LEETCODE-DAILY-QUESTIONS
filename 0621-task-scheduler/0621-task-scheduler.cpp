6c783c27ffefd29f1da3a49022cd125a1459f6b5
RITWIK RAI <rritwik1810@gmail.com>
2025-11-19 14:32:14 +0530

    Time: 0 ms (100%), Space: 38.2 MB (83.05%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -2,63 +2,44 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
 
-          int size = tasks.size();
+         int size = tasks.size();
 
-          vector<int>freq(26,0);
+         vector<int>freq(26,0);
 
-          for(int i=0;i<size;i++)
-          {
-              freq[tasks[i]-'A']++;
-          }
+         int gaddha = 0;
 
-          priority_queue< int >pq;
+         for(int i = 0 ; i < size ; i++)
+         {
+             freq[tasks[i]-'A']++;
+         }
 
-          for(int i=0;i<26;i++)
-          {
+         vector<int>freq1;
+
+         for(int i=0;i<26;i++)
+         {
              if(freq[i]!=0)
              {
-                 pq.push(freq[i]);
+                 freq1.push_back(freq[i]);
              }
-          }
+         }
 
-          int tot_pop=n+1;
+         sort(freq1.begin(),freq1.end());
 
-          int ans=0;
+         int val=freq1[freq1.size()-1];
 
-          int last=0;
+         gaddha=val-1;
 
-          while(!pq.empty())
-          {
-              vector<int>temp;
+         int idle=(gaddha*n);
 
-              int a=0;
+         for(int i=freq1.size()-2;i>=0;i--)
+         {
+             idle-=min(gaddha,freq1[i]);
+         }
 
-              int size1=pq.size();
+         if(idle>0)
+           return tasks.size() + idle;
 
-              while(a!=min(tot_pop,size1))
-              {
-                  temp.push_back(pq.top());
+        return tasks.size();
 
-                  pq.pop();
-                  
-                  a++;
-              }
-
-              ans+=a+(tot_pop-a);
-
-              for(int i=0;i<temp.size();i++)
-              {
-                  int val=temp[i]-1;
-
-                  if(val>0)
-                  pq.push(val);
-              }
-
-              last=a;
-
-          }
-
-       return ans-(tot_pop-last);
-        
     }
 };
