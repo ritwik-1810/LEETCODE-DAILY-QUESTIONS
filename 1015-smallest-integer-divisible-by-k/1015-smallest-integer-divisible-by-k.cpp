ccc102d7e62f0d6456a3998fe957f156e0cda924
RITWIK RAI <rritwik1810@gmail.com>
2025-11-25 13:50:32 +0530

    Time: 0 ms (100%), Space: 7.8 MB (52.58%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
@@ -2,6 +2,20 @@ class Solution {
 public:
     int smallestRepunitDivByK(int k) {
 
+
+        //note 
+
+        /*
+
+        first we try to build the brute force approach then it does not work so we 
+
+        optimize a bit because n may not fit in a 64-bit signed integer. 
+
+        so we use remainder.. by doing %k;
+
+
+        */
+
         long long n=1;
 
         if(k%10!=1 && k%10!=3 && k%10!=7 && k%10!=9)
