5b3fefb9e0d4b1e93cf8e510155260295b673cec
RITWIK RAI <rritwik1810@gmail.com>
2025-11-27 23:43:25 +0530

    Time: 0 ms (100%), Space: 38.1 MB (95.25%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -2,44 +2,41 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
 
-         int size = tasks.size();
+        int size = tasks.size();
 
-         vector<int>freq(26,0);
+        vector<int>freq(26,0);
 
-         int gaddha = 0;
+        for(int i=0;i<size;i++)
+        {
+            freq[tasks[i]-'A']++;
+        }
 
-         for(int i = 0 ; i < size ; i++)
-         {
-             freq[tasks[i]-'A']++;
-         }
+        sort(freq.begin(),freq.end());
 
-         vector<int>freq1;
+        vector<int>vec;
 
-         for(int i=0;i<26;i++)
-         {
-             if(freq[i]!=0)
-             {
-                 freq1.push_back(freq[i]);
-             }
-         }
+        for(int i=25;i>=0;i--)
+        {
+            if(freq[i]!=0)
+              vec.push_back(freq[i]);
+        }
 
-         sort(freq1.begin(),freq1.end());
+        int fr=vec[0];
 
-         int val=freq1[freq1.size()-1];
+        int gaddha=fr-1;
 
-         gaddha=val-1;
+        int place=gaddha*n;
 
-         int idle=(gaddha*n);
+        for(int i=1;i<vec.size();i++)
+        {
+            int val=min(gaddha,vec[i]);
 
-         for(int i=freq1.size()-2;i>=0;i--)
-         {
-             idle-=min(gaddha,freq1[i]);
-         }
+            place-=val;
+        }
 
-         if(idle>0)
-           return tasks.size() + idle;
+        if(place>0) return tasks.size()+place;
 
         return tasks.size();
-
+        
     }
 };
