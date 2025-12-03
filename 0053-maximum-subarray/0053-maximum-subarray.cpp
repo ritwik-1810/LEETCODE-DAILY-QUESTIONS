a601c035fd03aaa589d3a27bfe11170281ab5d69
RITWIK RAI <rritwik1810@gmail.com>
2025-12-03 21:30:45 +0530

    Time: 5 ms (5.55%), Space: 74.5 MB (5.03%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
+    int solve(int i, vector<int>& nums, vector<int>& memo) {
+    // Base case
+    if (i == 0)
+        return memo[i] = nums[i];
+
+    // If already computed
+    if (memo[i] != INT_MIN)
+        return memo[i];
+
+    // Recurrence:
+    // Max subarray ending at i is either:
+    // 1. Start new subarray at i
+    // 2. Extend previous subarray
+    int extend = solve(i - 1, nums, memo) + nums[i];
+    int startNew = nums[i];
+
+    memo[i] = max(startNew, extend);
+    return memo[i];
+}
     int maxSubArray(vector<int>& nums) {
 
-        /*Simple Logic of kadane algo is ---> currmax = max(current element , abhi tak ka sum),
-           
-          keep updating ans=max(previous max , currmax);
+         int n = nums.size();
+    vector<int> memo(n, INT_MIN);
 
-        */
+    int ans = INT_MIN;
 
-        int curr=0;
+    // Evaluate dp[i] for all i
+    for (int i = 0; i < n; i++) {
+        ans = max(ans, solve(i, nums, memo));
+    }
 
-        int ans=INT_MIN;
-
-        for(auto c:nums)
-        {
-            curr=max(c,curr+c);
-
-            ans=max(ans,curr);
-        }
-
-        return ans;
+    return ans;
+        
     }
 };
