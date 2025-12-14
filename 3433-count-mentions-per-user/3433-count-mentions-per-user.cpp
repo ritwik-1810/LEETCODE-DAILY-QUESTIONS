985e32862b167960fdd3c36ebb861423f8d36095
RITWIK RAI <rritwik1810@gmail.com>
2025-12-14 14:00:19 +0530

    Time: 67 ms (15.53%), Space: 58.1 MB (13.09%) - LeetHub

--- 3433-count-mentions-per-user/3433-count-mentions-per-user.cpp
+++ 3433-count-mentions-per-user/3433-count-mentions-per-user.cpp
@@ -5,14 +5,13 @@ public:
        int t1 = stoi(a[1]);
        int t2 = stoi(b[1]);
 
-    if (t1 == t2)
-        return a[0][0] >  b[0][0];
+       if (t1 == t2)
+         return a[0][0]  >  b[0][0];
 
-    return t1 < t2;
+       return t1 < t2;
     }
     void changeMention(vector<string>& eve,set<int>&st, vector<int>&mentions)
     {
-
         if(eve[2]=="ALL")
         {
             for(int i=0;i<mentions.size();i++)
@@ -31,14 +30,15 @@ public:
         {
             stringstream ss(eve[2]);
 
-        string token;
+            string token;
 
-        vector<string>ids;
+            vector<string>ids;
 
-        while(ss >> token)
-        {
-           ids.push_back(token);
-        }
+            while(ss >> token)
+            {
+              ids.push_back(token);
+            }
+
             for(int i=0;i<ids.size();i++)
             {
                 mentions[stoi(ids[i].substr(2))]++;
@@ -51,43 +51,33 @@ public:
 
         vector<int>mentions(numberOfUsers,0);
 
-        //queue for storing the offline
-
         queue<pair<int,int>>qt;
 
-        //set for storing the online..
-
         set<int>st;
-
-        //initially all are active so push all in active..
 
         for(int i=0;i<numberOfUsers;i++)
         {
             st.insert(i);
         }
 
-        //sort the events vector based on events timestamps, if timestamp is same the offline first..
-
         sort(begin(events),end(events),mycomp);
-
-        //traverse the events...
 
         for(int i = 0 ; i < size ; i++)
         {
-            int tt=stoi(events[i][1]);
+             int tt=stoi(events[i][1]);
 
-                 while(!qt.empty() && qt.front().second <= tt)
-                 {
+              while(!qt.empty() && qt.front().second <= tt)
+                {
                      auto [id,t]=qt.front();
 
                      qt.pop();
 
                      st.insert(id);
-                 }
+                }
+           
             if(events[i][0]=="MESSAGE")
             {
-
-                 changeMention(events[i],st,mentions);
+                changeMention(events[i],st,mentions);
             }
             else
             {
@@ -100,8 +90,7 @@ public:
             }
         }
 
-
         return mentions;
-        
+
     }
 };
