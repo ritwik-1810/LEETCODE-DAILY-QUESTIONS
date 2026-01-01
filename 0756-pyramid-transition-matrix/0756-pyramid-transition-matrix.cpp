f142abfa1954560a3c5534f3f8a2f7ad9e237be3
RITWIK RAI <rritwik1810@gmail.com>
2026-01-01 12:09:58 +0530

    Time: 2292 ms (5.01%), Space: 11.9 MB (84.11%) - LeetHub

--- 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.cpp
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.cpp
@@ -1,53 +1,49 @@
 class Solution {
+    // Map to store allowed transitions
+    // Key: pair of characters (length 2)
+    // Value: list of possible characters on top
+    unordered_map<string, vector<char>> mpp;
+
 public:
-    unordered_map<string , bool>dp;
-
-    bool solve(string curr , int i , unordered_map<string,vector<char>>&mp,string upper)
-    {
-         if(curr.size()==1)
-         {
+    // DFS function to try building pyramid
+    // bot -> current bottom row
+    // i   -> current index in bot
+    // tem -> temporary string for next row
+    bool dfs(string bot, int i, string tem) {
+        
+        // Base case: pyramid successfully built
+        if (bot.size() == 1) 
             return true;
-         }
 
-          string key=curr + '|' + to_string(i) + '|' + upper;
-
-         if(dp.count(key)) return dp[key];
-
-         if(i==curr.size()-1)
-         {
-            return dp[key]=solve(upper,0,mp,"");
-         }
-
-         string s=curr.substr(i,2);
-
-         if(!mp.count(s)) return dp[key]=false;
-
-         for(auto ch:mp[s])
-         {
-             upper.push_back(ch); // do
-
-             if(solve(curr,i+1,mp,upper)) return dp[key]=true; //explore
-
-             upper.pop_back(); // backtrack
-         }
-
-         return dp[key] = false;
-         
-    }
-    bool pyramidTransition(string bottom, vector<string>& allowed) {
-
-        unordered_map<string,vector<char>>mp;
-
-        int size = allowed.size();
-
-        for(int i=0;i<size;i++)
-        {
-            string s = allowed[i].substr(0,2);
-
-            mp[s].push_back(allowed[i][2]);
+        // If we finished building the next row
+        if (i == bot.size() - 1) {
+            string nextRow;
+            return dfs(tem, 0, nextRow);
         }
 
-        return solve(bottom,0,mp,"");
+        // Take current adjacent pair
+        string key = bot.substr(i, 2);
+
+        // Try all possible characters that can go above this pair
+        for (char v : mpp[key]) {
+            tem.push_back(v);              // choose
+            if (dfs(bot, i + 1, tem))      // explore
+                return true;               // valid pyramid found
+            tem.pop_back();                // backtrack
+        }
+
+        // No valid configuration found
+        return false;
+    }
+
+    bool pyramidTransition(string bottom, vector<string>& allowed) {
         
+        // Build the mapping from allowed transitions
+        for (auto &a : allowed) {
+            mpp[a.substr(0, 2)].push_back(a[2]);
+        }
+
+        string temp;
+        return dfs(bottom, 0, temp);
     }
 };
