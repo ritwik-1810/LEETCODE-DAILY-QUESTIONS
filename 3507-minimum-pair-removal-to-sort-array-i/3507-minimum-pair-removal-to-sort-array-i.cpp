f0d7b926a341a775c2dc33f0a476672a4dd00e80
RITWIK RAI <rritwik1810@gmail.com>
2026-01-25 03:09:43 +0530

    Time: 3 ms (35.68%), Space: 27.1 MB (37.65%) - LeetHub

--- 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
@@ -1,56 +1,34 @@
 class Solution {
 public:
-    bool isSorted(vector<int>&arr)
-    {
-        for(int i=0;i<arr.size()-1;i++)
-        {
-            if(arr[i]>arr[i+1]) return false;
+    bool isSorted(vector<int>& arr) {
+        for (int i = 0; i < arr.size() - 1; i++) {
+            if (arr[i] > arr[i + 1]) return false;
         }
-
         return true;
     }
+
     int minimumPairRemoval(vector<int>& nums) {
+        vector<int> arr = nums;
+        int cnt = 0;
 
-        vector<int>arr=nums;
+        while (!isSorted(arr)) {
+            int idx = -1;
+            int mini = INT_MAX;
 
-        int cnt=0;
-
-        while(true)
-        {
-            if(isSorted(arr)) return cnt;
-
-            vector<int>n;
-
-            cnt+=1;
-
-            int mini=INT_MAX;
-
-            int idx=-1;
-
-            for(int i=0;i<arr.size()-1;i++)
-            {
-                if((arr[i]+arr[i+1])<mini)
-                {
-                    idx=i;
-
-                    mini=arr[i]+arr[i+1];
+            // find minimum adjacent pair
+            for (int i = 0; i < arr.size() - 1; i++) {
+                if (arr[i] + arr[i + 1] < mini) {
+                    mini = arr[i] + arr[i + 1];
+                    idx = i;
                 }
             }
 
-            arr[idx]=mini;
-
-            arr[idx+1]=INT_MIN;
-
-            for(int i=0;i<arr.size();i++)
-            {
-                if(arr[i]!=INT_MIN)
-                n.push_back(arr[i]);
-            }
-
-            arr=n;
+            // merge pair
+            arr[idx] = mini;
+            arr.erase(arr.begin() + idx + 1);
+            cnt++;
         }
 
         return cnt;
-        
     }
 };
