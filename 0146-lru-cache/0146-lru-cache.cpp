b85b88e8dd058e175568c4b0909751c9a80524a6
RITWIK RAI <rritwik1810@gmail.com>
2026-02-08 00:07:10 +0530

    Time: 289 ms (5.03%), Space: 172.1 MB (95.13%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,68 @@
+class LRUCache {
+public:
+    unordered_map<int,int>mp;
+    unordered_map<int,int>pp;
+    int size,prev;
+    LRUCache(int capacity) {
+
+      size=capacity;
+      prev=0;
+        
+    }
+    
+    int get(int key) {
+
+        if(mp.find(key)!=mp.end())
+        {
+          prev++;
+
+          pp[key]=prev;
+
+          return mp[key];
+        }
+
+        return -1;
+    }
+    
+    void put(int key, int value) {
+
+       
+        mp[key]=value;
+
+        prev++;
+
+        pp[key]=prev;
+
+         if(mp.size()>size)
+        {
+            int val=INT_MAX;
+
+            int kk;
+
+            for(auto v:pp)
+            {
+                if(val>v.second)
+                {
+                    val=v.second;
+
+                    kk=v.first;
+                }
+            }
+
+            mp.erase(kk);
+
+            pp.erase(kk);
+        }
+
+
+        
+        
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
