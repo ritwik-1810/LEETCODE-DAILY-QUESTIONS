66e4b6a1d319f862f5f6061c411f66ccdc98f27c
RITWIK RAI <rritwik1810@gmail.com>
2026-06-19 03:39:58 +0530

    Time: 30 ms (44.56%), Space: 23.9 MB (79.47%) - LeetHub

---
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    char processStr(string s, long long k) {
+
+        int size = s.size();
+
+        long long L=0;
+
+        for(int i=0;i<size;i++)
+        {
+            if(s[i]>='a' && s[i]<='z')
+            {
+                L++;
+            }
+            else if(s[i]=='#')
+            {
+                L+=L;
+            }
+            else if(s[i]=='*')
+            {
+                if(L>0)
+                L--;
+            }
+
+        }
+
+        if(k>=L) return '.';
+
+        for(int i=size-1;i>=0;i--)
+        {
+            if(s[i]=='*')
+            {
+                L++;
+            }
+            else if(s[i]=='%')
+            {
+                k=L-k-1;
+            }
+            else if(s[i]=='#')
+            {
+                L=L/2;
+                
+                if(k>=L)
+                k=(k-L);
+            }
+            else if(s[i]>='a' && s[i]<='z')
+            {
+                L--;
+
+                 if(k==L)
+                   return s[i];
+            }
+
+           
+        }
+
+        return '.';
+        
+    }
+};
