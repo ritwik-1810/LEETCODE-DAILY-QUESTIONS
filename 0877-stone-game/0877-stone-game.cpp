77f8c09a7629ee4c18cae95158c8e849bd1d64b3
RITWIK RAI <rritwik1810@gmail.com>
2026-08-03 01:27:07 +0530

    Time: 0 ms (100%), Space: 10.4 MB (87.9%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,44 +1,8 @@
 class Solution {
 public:
-        int solve(vector<int>&nums,int i,int j,vector<vector<int>>&dp)
-    {
-        if(i>j)
-        {
-            return 0;
-        }
-
-        if(i==j)
-        {
-            return nums[i];
-        }
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        int take_i=nums[i] + min(solve(nums,i+2,j,dp),solve(nums,i+1,j-1,dp));
-
-        int take_j=nums[j] + min(solve(nums,i,j-2,dp),solve(nums,i+1,j-1,dp));
-
-        return dp[i][j] = max(take_i,take_j);
-       
-    }
-        
     bool stoneGame(vector<int>& piles) {
 
-        int size = piles.size();
-
-        int i=0;
-
-        int j=size-1;
-
-        vector<vector<int>>dp(size,vector<int>(size,-1));
-
-        int sum = accumulate(piles.begin(),piles.end(),0);
-       
-        int aliceSum=solve(piles,0,j,dp);
-
-        if((sum-aliceSum)>aliceSum) return false;
-
         return true;
-
+        
     }
 };
