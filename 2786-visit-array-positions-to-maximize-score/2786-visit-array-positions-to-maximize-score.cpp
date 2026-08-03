ae9442e47f017e5dce093a4fc19579c1e81a292b
RITWIK RAI <rritwik1810@gmail.com>
2026-08-03 15:09:22 +0530

    Time: 39 ms (60.23%), Space: 121.2 MB (50%) - LeetHub

--- 2786-visit-array-positions-to-maximize-score/2786-visit-array-positions-to-maximize-score.cpp
+++ 2786-visit-array-positions-to-maximize-score/2786-visit-array-positions-to-maximize-score.cpp
@@ -1,84 +1,53 @@
 class Solution {
 public:
-    long long solve(vector<int>&nums , int i , int x , vector<long long>&dp,vector<int>&even,vector<int>&odd)
-    {
-        if(i>=nums.size())
-        {
-            return 0;
-        }
-
-        if(dp[i]!=-1) return dp[i];
-
-        long long diff=0;
-
-        long long same=0;
-
-        int j=i+1;
-
-        int k=i+1;
-
-        if(nums[i]%2==0)
-        {
-           j=even[i+1];
-
-           k=odd[i+1];
-        }
-        else
-        {
-           k=even[i+1];
-
-           j=odd[i+1];
-        }
-        
-        if(j<nums.size())
-        same = nums[j] + solve(nums,j,x,dp,even,odd);
-
-        if(k<nums.size())
-        {
-            diff = nums[k] - x + solve(nums,k,x,dp,even,odd);
-        }
-
-        return dp[i] = max(same,diff);
-    }
     long long maxScore(vector<int>& nums, int x) {
 
-        int size = nums.size();
+          int n = nums.size();
 
-        vector<long long>dp(size,-1);
+        vector<int> even(n + 1, n);
+        vector<int> odd(n + 1, n);
 
-        vector<int>even(size+1,0);
-
-        even[size]=size;
-
-        for(int i=size-1;i>=0;i--)
-        {
-            if(nums[i]%2==0)
-            {
-                even[i]=i;
-            }
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] % 2 == 0)
+                even[i] = i;
             else
-            {
-                even[i]=even[i+1];
-            }
+                even[i] = even[i + 1];
         }
 
-        vector<int>odd(size+1,0);
-
-        odd[size]=size;
-
-        for(int i=size-1;i>=0;i--)
-        {
-            if(nums[i]%2!=0)
-            {
-                odd[i]=i;
-            }
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] % 2)
+                odd[i] = i;
             else
-            {
-                odd[i]=odd[i+1];
-            }
+                odd[i] = odd[i + 1];
         }
 
-        return nums[0] + solve(nums,0,x,dp,even,odd);
+        vector<long long> dp(n + 1, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            long long same = 0;
+            long long diff = 0;
+
+            int j, k;
+
+            if (nums[i] % 2 == 0) {
+                j = even[i + 1];
+                k = odd[i + 1];
+            } else {
+                j = odd[i + 1];
+                k = even[i + 1];
+            }
+
+            if (j < n)
+                same = nums[j] + dp[j];
+
+            if (k < n)
+                diff = nums[k] - x + dp[k];
+
+            dp[i] = max(same, diff);
+        }
+
+        return nums[0] + dp[0];
         
     }
 };
